ecdd44ac06d9b143d77c559f281982648a9f521f
Siddharth <siddharthrajaram.ingale231@gmail.com>
2026-02-13 21:05:39 +0530

    Added mysql database configuration

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -7,12 +7,12 @@ import org.testng.annotations.Test;
 public class LoginTest extends BaseTest {
 
     @Test
-    public void loginTest() {
+    public void loginTest() throws InterruptedException {
 
         // Always get driver via getter
         getDriver().get("https://google.com");
-
+        Thread.sleep(2000);
         // Intentionally broken XPath (for self-healing later)
-        getDriver().findElement(By.xpath("//a[@aria-label='Gmai ']")).click();
+        getDriver().findElement(By.xpath("//a[@aria-label='Gmail and save']")).click();
     }
 }
